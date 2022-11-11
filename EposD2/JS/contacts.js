e08165a86bc5a19e1e29e8be45f2c1725a8ab694
SinhroLK <lkrstic001@gmail.com>
2022-11-11 20:32:56 +0100

    NIGHTMARE NIGHTMARE NIGHTMARE NIGHTMARE NIGHTMARE NIGHTMARE

--- EposD2/JS/contacts.js
+++ EposD2/JS/contacts.js
@@ -35,24 +35,23 @@ function prikaziGithub(){
     }
 }
 
-function faq(){
-    var faq = document.getElementsByClassName("q");
-    var i;
-    for (i = 0; i < faq.length; i++) {
-        faq[i].addEventListener("click", function () {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
-            this.classList.toggle("active");
-            /* Toggle between hiding and showing the active panel */
-            var body = this.nextElementSibling;
-            if (getComputedStyle(body, null) === 'block') {
-                body.style.display = "none";
-            } else {
-                body.style.display = "block";
-            }
-        });
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
     }
-}
+  }
+  
+  window.addEventListener("scroll", reveal);
 
 
 // let mylogo1 = document.getElementById('about_us');
